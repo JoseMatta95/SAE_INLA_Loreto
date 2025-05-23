72fa7bec6e2f225d2dbc55c7cb93c2b230e517c9
Jose Matta <josematta@MacBook-Pro-de-Jose.local>
2025-05-23 01:10:58 -0500

    Graph for evaluating department-level aggregated estimates for Loreto

--- 03.SAE_INLA_IIpart_EDU-count.R
+++ 03.SAE_INLA_IIpart_EDU-count.R
@@ -548,31 +548,153 @@ graficos<-
 
 graficos$graph[[8]]
   
-  # seguir generando
-  
-  arrange(year,ubigeo,name) %>% 
-  group_by(year, ubigeo) %>% 
-  mutate(grupo = row_number()) %>%
-  ungroup() %>% 
-  
-  group_by(year, ubigeo) %>%
-  # Repetir el grupo del fit en el real_cases correspondiente
-  mutate(grupo = min(grupo[name != "real_cases"])) %>%
-  ungroup()
-  
-  ggplot(aes(x = value, ubigeo))+
-  geom_point(aes(col = name))+
-  geom_line(aes(group = ubigeo))+
-  
-  facet_grid(grupo~year)
   
   
-  # calculation proportions with cases predicted
+# calculation proportions with cases predicted
   
+prop_sae_predict_edu<-
   distritos_sf_predict %>% 
-    select(real_cases,fit2_spatial,fit2_new,fit5_spat,pop_landscan) %>% 
-    mutate(
-      across(.cols = c(real_cases:fit5_spat), .f= ~round(.x/pop_landscan))
-    )
+  select(year,ubigeo,pop_landscan,real_cases,fit10_spat,
+         fit8_spat,fit5_spat,fit2_spatial,fit2_new,geometry) %>% 
+  mutate(
+    across(.cols = c(real_cases:fit2_new), .f=~.x/pop_landscan)
+  )
+
+
+prop_sae_predict_edu %>% 
+  pivot_longer(cols = real_cases:fit2_new) %>% 
+  mutate(
+    name = factor(name, levels = c("real_cases", "fit2_new", "fit2_spatial", "fit5_spat", 
+                                   "fit8_spat", "fit10_spat"))
+  ) %>% 
   
+  ggplot()+
+  geom_sf(aes(fill = value)) +
+  scale_fill_viridis(direction = -1,option = "rocket") +
+  facet_grid(name~year)
+
+
+# Point and interval estimates of proportions (INLA)
+
+modelos <- list( # lista para trabajar en bloque
+  modelo_fh8 = modelo_fh8,
+  modelo_fh8_spatial = modelo_fh8_spatial,
+  modelo_fh10 = modelo_fh10,
+  modelo_fh10_spatial = modelo_fh10_spatial,
+  modelo_fh5 = modelo_fh5,
+  modelo_fh5_spatial = modelo_fh5_spatial,
+  modelo_fh2 = modelo_fh2,
+  modelo_fh2_spatial = modelo_fh2_spatial,
+  modelo_fh2_new = modelo_fh2_spatial_new
+)
+
+resultados <- imap_dfr(modelos, ~{ # formato long de las estimaciones y los IC de cada modelo distrito-año
+  data.frame(
+    year = edu_censo_final$year,
+    ubigeo = edu_censo_final$ubigeo,
+    mean = .x$summary.fitted.values$mean,
+    lower = .x$summary.fitted.values$`0.025quant`,
+    upper = .x$summary.fitted.values$`0.975quant`,
+    modelo = .y
+  )
+}) 
+
+
+resultados %>% 
+  ggplot(aes(x = mean, y = ubigeo, col = modelo))+
+  geom_point()+
+  geom_errorbar(aes(xmin = lower, xmax = upper))+
+  facet_grid(modelo~year)
+
+
+# bottom-up? benchmarking? validación por agregación?
+
+# 1. Estimates will be calculated for each year for the whole Loreto region, 2. The district-level estimates 
+# by year will be aggregated to the department-year level, 3. It will be verified if the direct estimate 
+# is on the confidence interval (CI) of the SAE-INLA model estimates.
+
+# The logic is that the true value is expected to lie within the CI of the model/design-based estimate.
+# ENDES assumes that the true value lies within its own CI. For the SAE-INLA models, we would expect 
+# ENDES estimates to fall within the model-generated CIs.
+
+
+# 1. Estimates will be calculated for each year for the whole Loreto region
+
+data_final_nest<-
+  wi_edu_2010_2020 %>% 
+  group_by(year) %>% 
+  nest() %>% 
+  mutate(
+    svydesign_endes= map(.x=data,
+                         .f = ~svydesign(ids = ~hv001, strata = ~hv022, 
+                                         weights = ~hv005, data = .x, nest = T)),
+    
+    wi_directa = map(.x = svydesign_endes,
+                     .f = ~svyby(~hv270, ~hv023, design = .x, svymean, keep.var = T)),
+    
+    edu_directa = map(.x = svydesign_endes,
+                      .f = ~svyby(~hv109_recat, ~hv023, design = .x, svymean, keep.var = T)))
+
+
+# 2. The district-level estimates by year will be aggregated to the department-year level
+
+predict_sae_loreto<-
+  resultados %>% 
+  full_join(pop_dist) %>% # total population for calculate proportions
+  group_by(year,modelo) %>% 
+  summarise(
+    total_loreto = sum(pop_landscan),
+    mean_loreto = sum(mean),
+    lower_loreto = sum(lower),
+    upper_loreto = sum(upper)
+  ) %>% 
+  mutate(
+    across(.cols = c(mean_loreto:upper_loreto), .f = ~.x/total_loreto)
+  )
+
+
+
+
+#3. It will be verified if the direct estimate is on the confidence interval (CI) of 
+# the SAE-INLA model estimates
+
+data_final_nest %>% 
+  select(year,edu_directa) %>% 
+  unnest(edu_directa) %>% 
+  ungroup() %>%
+  select(year,hv109_recat) %>% 
   
+  full_join(predict_sae_loreto) %>% 
+  
+  mutate(
+    check = ifelse(hv109_recat > lower_loreto & hv109_recat<upper_loreto, "cumple", "no cumple")
+  ) %>% 
+  
+  ggplot()+
+  geom_pointrange(aes(x = modelo, y =mean_loreto,
+                      col = modelo, ymin = lower_loreto, ymax = upper_loreto))+
+  geom_point(aes(x = modelo, y = hv109_recat, shape = check), col = 'black', show.legend = F)+
+  
+  scale_shape_manual(
+    values = c(
+      "cumple" = 23, 
+      "no cumple" = 15)
+  ) +
+  
+  coord_flip()+
+  facet_grid(rows = vars(year), switch = "y") +
+  
+  theme_minimal() +
+  
+  theme(
+    panel.grid.major=element_blank(),
+    panel.grid.minor=element_blank(),
+    axis.text.y=element_blank(),
+    axis.line=element_line(),
+    strip.placement = "outside",
+    axis.text.x=element_text(face="bold"),
+    strip.text.y = element_text(vjust = 1,size = 10,face="bold"))
+
+
+
+
